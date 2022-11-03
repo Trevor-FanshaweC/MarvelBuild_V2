381d2372e31410ef6f2d74cfd4d7072a99d712c8
Trevor - Fanshawe <tvanrys@fanshaweonline.ca>
2022-11-03 08:53:12 -0400

    working

--- js/main.js
+++ js/main.js
@@ -1,15 +1,37 @@
+// import components first!!
+import HeroThumb from './components/TheHeroThumbnail.js';
+import LightBox from './components/TheLightbox.js';
+
 (() => {
     // create vue instance here
     const { createApp } = Vue
 
     createApp({
         created() {
-            
+            // ALWAYS get your remote data here
+            fetch('./data.json')
+                .then(res => res.json())
+                .then(data => this.heroData = data)
+            .catch(error => console.error(error));
         },
 
         data() {
             return {
-                heroData: {}
+                heroData: {},
+                lbData: {},
+                showLB: false
+            }
+        },
+
+        components: {
+            theherothumb: HeroThumb,
+            thelightbox: LightBox
+        },
+
+        methods: {
+            loadLightbox(hero) {
+                this.lbData = hero;
+                this.showLB = true;
             }
         }
     }).mount('#app')
